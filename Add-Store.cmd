3cd3cb5cd7cbe194dae73e5f0c9cb2785d222b0e
carlostcba <33902391+carlostcba@users.noreply.github.com>
2024-06-01 00:13:50 -0300

    Update Add-Store.cmd

--- Add-Store.cmd
+++ Add-Store.cmd
@@ -1,128 +1,116 @@
 @echo off
+REM Comprobar la versión de Windows
 for /f "tokens=6 delims=[]. " %%G in ('ver') do if %%G lss 16299 goto :version
+
+REM Comprobar los permisos de administrador
 %windir%\system32\reg.exe query "HKU\S-1-5-19" 1>nul 2>nul || goto :uac
+
+REM Habilitar las extensiones locales
 setlocal enableextensions
+
+REM Determinar la arquitectura del procesador
 if /i "%PROCESSOR_ARCHITECTURE%" equ "AMD64" (set "arch=x64") else (set "arch=x86")
 cd /d "%~dp0"
 
+REM Comprobar la existencia de archivos necesarios
 if not exist "*WindowsStore*.appxbundle" goto :nofiles
 if not exist "*WindowsStore*.xml" goto :nofiles
 
-for /f %%i in ('dir /b *WindowsStore*.appxbundle 2^>nul') do set "Store=%%i"
-for /f %%i in ('dir /b *NET.Native.Framework*1.6*.appx 2^>nul ^| find /i "x64"') do set "Framework6X64=%%i"
-for /f %%i in ('dir /b *NET.Native.Framework*1.6*.appx 2^>nul ^| find /i "x86"') do set "Framework6X86=%%i"
-for /f %%i in ('dir /b *NET.Native.Runtime*1.6*.appx 2^>nul ^| find /i "x64"') do set "Runtime6X64=%%i"
-for /f %%i in ('dir /b *NET.Native.Runtime*1.6*.appx 2^>nul ^| find /i "x86"') do set "Runtime6X86=%%i"
-for /f %%i in ('dir /b *VCLibs*140*.appx 2^>nul ^| find /i "x64"') do set "VCLibsX64=%%i"
-for /f %%i in ('dir /b *VCLibs*140*.appx 2^>nul ^| find /i "x86"') do set "VCLibsX86=%%i"
-
-if exist "*StorePurchaseApp*.appxbundle" if exist "*StorePurchaseApp*.xml" (
-for /f %%i in ('dir /b *StorePurchaseApp*.appxbundle 2^>nul') do set "PurchaseApp=%%i"
-)
-if exist "*DesktopAppInstaller*.appxbundle" if exist "*DesktopAppInstaller*.xml" (
-for /f %%i in ('dir /b *DesktopAppInstaller*.appxbundle 2^>nul') do set "AppInstaller=%%i"
-)
-if exist "*XboxIdentityProvider*.appxbundle" if exist "*XboxIdentityProvider*.xml" (
-for /f %%i in ('dir /b *XboxIdentityProvider*.appxbundle 2^>nul') do set "XboxIdentity=%%i"
-)
-
+REM Definir dependencias según la arquitectura
 if /i %arch%==x64 (
-set "DepStore=%VCLibsX64%,%VCLibsX86%,%Framework6X64%,%Framework6X86%,%Runtime6X64%,%Runtime6X86%"
-set "DepPurchase=%VCLibsX64%,%VCLibsX86%,%Framework6X64%,%Framework6X86%,%Runtime6X64%,%Runtime6X86%"
-set "DepXbox=%VCLibsX64%,%VCLibsX86%,%Framework6X64%,%Framework6X86%,%Runtime6X64%,%Runtime6X86%"
-set "DepInstaller=%VCLibsX64%,%VCLibsX86%"
+    set "Dependencias=%VCLibsX64%,%VCLibsX86%,%Framework6X64%,%Framework6X86%,%Runtime6X64%,%Runtime6X86%"
 ) else (
-set "DepStore=%VCLibsX86%,%Framework6X86%,%Runtime6X86%"
-set "DepPurchase=%VCLibsX86%,%Framework6X86%,%Runtime6X86%"
-set "DepXbox=%VCLibsX86%,%Framework6X86%,%Runtime6X86%"
-set "DepInstaller=%VCLibsX86%"
+    set "Dependencias=%VCLibsX86%,%Framework6X86%,%Runtime6X86%"
 )
 
-for %%i in (%DepStore%) do (
-if not exist "%%i" goto :nofiles
+REM Verificar la existencia de dependencias
+for %%i in (%Dependencias%) do (
+    if not exist "%%i" goto :nofiles
 )
 
-set "PScommand=PowerShell -NoLogo -NoProfile -NonInteractive -InputFormat None -ExecutionPolicy Bypass"
-
+REM Instalación de aplicaciones
 echo.
 echo ============================================================
-echo Adding Microsoft Store
+echo Agregando Microsoft Store
 echo ============================================================
 echo.
-1>nul 2>nul %PScommand% Add-AppxProvisionedPackage -Online -PackagePath %Store% -DependencyPackagePath %DepStore% -LicensePath Microsoft.WindowsStore_8wekyb3d8bbwe.xml
-for %%i in (%DepStore%) do (
-%PScommand% Add-AppxPackage -Path %%i
+1>nul 2>nul %PScommand% Add-AppxProvisionedPackage -Online -PackagePath %Store% -DependencyPackagePath %Dependencias% -LicensePath Microsoft.WindowsStore_8wekyb3d8bbwe.xml
+for %%i in (%Dependencias%) do (
+    %PScommand% Add-AppxPackage -Path %%i
 )
 %PScommand% Add-AppxPackage -Path %Store%
 
+REM Instalación de otras aplicaciones si están definidas
 if defined PurchaseApp (
-echo.
-echo ============================================================
-echo Adding Store Purchase App
-echo ============================================================
-echo.
-1>nul 2>nul %PScommand% Add-AppxProvisionedPackage -Online -PackagePath %PurchaseApp% -DependencyPackagePath %DepPurchase% -LicensePath Microsoft.StorePurchaseApp_8wekyb3d8bbwe.xml
-%PScommand% Add-AppxPackage -Path %PurchaseApp%
+    echo.
+    echo ============================================================
+    echo Agregando Aplicación de Compra en la Tienda
+    echo ============================================================
+    echo.
+    1>nul 2>nul %PScommand% Add-AppxProvisionedPackage -Online -PackagePath %PurchaseApp% -DependencyPackagePath %Dependencias% -LicensePath Microsoft.StorePurchaseApp_8wekyb3d8bbwe.xml
+    %PScommand% Add-AppxPackage -Path %PurchaseApp%
 )
 if defined AppInstaller (
-echo.
-echo ============================================================
-echo Adding App Installer
-echo ============================================================
-echo.
-1>nul 2>nul %PScommand% Add-AppxProvisionedPackage -Online -PackagePath %AppInstaller% -DependencyPackagePath %DepInstaller% -LicensePath Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.xml
-%PScommand% Add-AppxPackage -Path %AppInstaller%
+    echo.
+    echo ============================================================
+    echo Agregando Instalador de Aplicaciones
+    echo ============================================================
+    echo.
+    1>nul 2>nul %PScommand% Add-AppxProvisionedPackage -Online -PackagePath %AppInstaller% -DependencyPackagePath %Dependencias% -LicensePath Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.xml
+    %PScommand% Add-AppxPackage -Path %AppInstaller%
 )
 if defined XboxIdentity (
-echo.
-echo ============================================================
-echo Adding Xbox Identity Provider
-echo ============================================================
-echo.
-1>nul 2>nul %PScommand% Add-AppxProvisionedPackage -Online -PackagePath %XboxIdentity% -DependencyPackagePath %DepXbox% -LicensePath Microsoft.XboxIdentityProvider_8wekyb3d8bbwe.xml
-%PScommand% Add-AppxPackage -Path %XboxIdentity%
+    echo.
+    echo ============================================================
+    echo Agregando Proveedor de Identidad de Xbox
+    echo ============================================================
+    echo.
+    1>nul 2>nul %PScommand% Add-AppxProvisionedPackage -Online -PackagePath %XboxIdentity% -DependencyPackagePath %Dependencias% -LicensePath Microsoft.XboxIdentityProvider_8wekyb3d8bbwe.xml
+    %PScommand% Add-AppxPackage -Path %XboxIdentity%
 )
+
+REM Fin del script
 goto :fin
 
 :uac
 echo.
 echo ============================================================
-echo Error: Run the script as administrator
+echo Error: Ejecute el script como administrador
 echo ============================================================
 echo.
 echo.
-echo Press any key to Exit
+echo Presione cualquier tecla para salir
 pause >nul
 exit
 
 :version
 echo.
 echo ============================================================
-echo Error: This pack is for Windows 10 version 1709 and later
+echo Error: Este paquete es para Windows 10 versión 1709 y posterior
 echo ============================================================
 echo.
 echo.
-echo Press any key to Exit
+echo Presione cualquier tecla para salir
 pause >nul
 exit
 
 :nofiles
 echo.
 echo ============================================================
-echo Error: Required files are missing in the current directory
+echo Error: Archivos requeridos no se encuentran en el directorio actual
 echo ============================================================
 echo.
 echo.
-echo Press any key to Exit
+echo Presione cualquier tecla para salir
 pause >nul
 exit
 
 :fin
 echo.
 echo ============================================================
-echo Done
+echo Hecho
 echo ============================================================
 echo.
-echo Press any Key to Exit.
+echo Presione cualquier tecla para salir.
 pause >nul
 exit
